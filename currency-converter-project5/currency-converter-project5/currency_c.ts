231ed7ab8c9cdf26010026fe5bd23bc2a4922ab2
Mehfooz Ali <168359047+mehfoozdev@users.noreply.github.com>
2024-05-02 22:02:57 +0500

    currency converter app

---
+++ currency-converter-project5/currency-converter-project5/currency_c.ts
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let condition : boolean = true;
+
+const currency: any = {
+    USD: 1,        // Base Currency             United States 
+    EUR: 0.94,     // 1 USD = 0.94 EUR          Euro 
+    GBP: 0.76,     // 1 USD = 0.76 GBP          British Pound Sterling
+    CAD: 1.25,     // 1 USD = 1.25 CAD          Canadian Dollar 
+    AUD: 1.32,     // 1 USD = 1.32 AUD          Australian Dollar 
+    NZD: 1.44,     // 1 USD = 1.44 NZD          New Zealand Dollar 
+    AED: 3.67,     // 1 USD = 3.76 AED          United Arab Emirates Dirham 
+    SAR: 3.75,     // 1 USD = 3.74 SAR          Saudi Riyal 
+    CNY: 6.40,     // 1 USD = 6.40 CNY          Chinese Yuan 
+    TRY: 13.5,     // 1 USD = 13.5 TRY          Turkish Lira 
+    INR: 75.50,    // 1 USD = 75.50 INR         Indian Rupee
+    PKR: 278.12    // 1 USD = 277.54 PKR        Pakistani Rupee
+};
+
+while (condition) {
+
+    let userAnswer = await inquirer.prompt(
+        [
+            {
+                name: "from",
+                message: chalk.black.bgYellowBright.underline.bold("\nEnter From Currency:"),
+                type: "list",
+                choices: ["USD", "EUR", "GBP", "CAD", "AUD", "NZD", "AED", "SAR", "CNY", "TRY", "INR", "PKR"]
+            },
+            {
+                name: "to",
+                message: chalk.black.bgYellowBright.underline.bold("\nEnter To Currency:"),
+                type: "list",
+                choices: ["USD", "EUR", "GBP", "CAD", "AUD", "NZD", "AED", "SAR", "CNY", "TRY", "INR", "PKR"]
+            },
+            {
+                name: "amount",
+                message: chalk.black.bgCyanBright.underline.bold("\nEnter Your Amount:"),
+                type: "number"
+            }
+        ]
+    );
+
+
+
+
+    // storing the currency of a "from" input in a variable
+    let fromAmount = currency[userAnswer.from];     // Dynamic Approach
+
+    // storing the currency of a "to" input in a variable
+    let toAmount = currency[userAnswer.to];
+
+    // storing the amount of a user input in a variable
+    let amount = userAnswer.amount;
+
+    // converting the amount in a USD because USD is our "Base Currency" 
+    let baseAmount = amount / fromAmount;
+
+    // converting in desired currency by just multiplying the "calculated USD (baseAmount)" with desired currency
+    let convertAmount = baseAmount * toAmount;
+
+    console.log(`${userAnswer.amount} ${userAnswer.from} to ${userAnswer.to} is: ${parseFloat(convertAmount.toFixed(2))}`);
+
+
+    // Continue or not
+    let askAnswer = await inquirer.prompt([{
+        name: "answer",
+        message: chalk.black.bgYellowBright.underline.bold("\nDo you want continue ? "),
+        default: 'false',
+        type: "confirm"
+
+    }]);
+
+    condition = askAnswer.answer
+
+
+} 
+
