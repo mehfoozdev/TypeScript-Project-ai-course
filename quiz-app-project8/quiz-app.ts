9d559c63263e38b960826d23eb2ce5b325ceffde
Mehfooz Ali <168359047+mehfoozdev@users.noreply.github.com>
2024-05-03 22:22:46 +0500

    quiz app - initial commit

---
+++ quiz-app-project8/quiz-app.ts
@@ -0,0 +1,10 @@
+// Quiz System
+
+
+
+// The app will show the students multiple choice questions and promt the user to reply. In the end it will show the students the result of the quiz.
+
+
+import inquirer from "inquirer"
+
+console.log('Quiz System')
